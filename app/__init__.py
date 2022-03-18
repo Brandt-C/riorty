bb6d2a276ce6462a0c77c4665771f4d2110a4034
Brandt-C <brandt.carlson11@gmail.com>
2022-03-18 10:18:11 -0500

    deploying to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 from config import Config
+from flask_cors import CORS
+
 
 from .user.routes import user 
 from .models import db, login
@@ -8,6 +10,8 @@ from flask_migrate import Migrate
 from .blog.routes import blog
 
 app = Flask(__name__)
+
+CORS(app, origins=["*"])
 
 app.config.from_object(Config)
 
